a79bbd72b03966eb269fb6c65c24930ed975f2f0
KyuWoo Choi <apple@paio.co.kr>
2014-07-11 06:00:57 +0900

    .

--- test/merged-test.js
+++ test/merged-test.js
@@ -2,24 +2,18 @@
 'use strict';
 
 var expect = expect || require('expect.js');
+var assert = require("assert");
 
 var merged = require('../lib/merged');
 
 describe('merged module', function () {
     describe('merge function', function () {
         it('should be implemented', function (done) {
-            merged.merge('./sample.js', 'http://localhost:8080/simepleTest', function callback(result) {
+            merged.merge(__dirname + '/res/sample.js', 'http://localhost:8080/simepleTest', function callback(result) {
                 expect(result).to.be.ok();
+                assert.ok(result);
                 done();
             });
-        });
-    });
-
-    describe('parse function', function () {
-        it('should be implemented', function (done) {
-            var ast = merged.parse(null);
-            expect(ast).to.be.ok();
-            done();
         });
     });
 
